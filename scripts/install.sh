20ad43614af17d92f492662294aea830badcce9d
Milad Rahmani <miladii_@hotmail.com>
2018-09-28 11:26:04 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,3 @@ curl -o Unity.pkg http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInst
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
-install "editor", "https://download.unity3d.com/download_unity/3071d1717b71/MacEditorInstaller/Unity-2018.2.5f1.pkg"
-install "windowssupport", "http://download.unity3d.com/download_unity/787658998520/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-2018.2.0f2.pkg"
-#install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
-install "linuxsupport", "http://download.unity3d.com/download_unity/787658998520/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-2018.2.0f2.pkg"
